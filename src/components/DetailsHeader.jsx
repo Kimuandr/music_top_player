6dcacd0851e3a40c75e0410adf531681bd5d2f1f
Kimuandr <kimuandr@gmail.com>
2022-10-31 18:14:57 +0300

    search and genres

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 
-const DetailsHeader = ({ artistId, artistData, songData }) => {
-  const artist = artistData?.artists[artistId].attributes;
+const DetailsHeader = ({ artistId, songData }) => {
+  const artist = songData?.artists[artistId].attributes;
   return (
     <div className="relative w-full flex flex-col">
       <div className="w-full bg-gradient-to-l from-transparent to-black sm:h48 h-28" />
